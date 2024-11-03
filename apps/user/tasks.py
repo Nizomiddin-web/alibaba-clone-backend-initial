748f05ae89d6792670b3569fb1f7e2f4c9d40dfc
Nizomiddin <reactspringgood@gmail.com>
2024-11-03 22:17:39 +0500

    change

--- apps/user/tasks.py
+++ apps/user/tasks.py
@@ -3,11 +3,12 @@ from celery import shared_task
 from django.core.mail import send_mail
 
 @shared_task
-def send_welcome(user_email):
+def send_email(user_email,otp_code):
     send_mail(
         'Xush kelibsiz!',
-        'Bizning saytga hush kelibsiz'
+        otp_code,
         'reactspringgood@gmail.com',
         [user_email],
         fail_silently=False
     )
+    return 200
--- core/settings.py
+++ core/settings.py
@@ -170,6 +170,13 @@ SPECTACULAR_SETTINGS = {
     # OTHER SETTINGS
 }
 
+# email
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default='')
+EMAIL_PORT = config('EMAIL_PORT', default='')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
 
 # jwt setup
