d14a769e6ca84463cbb71ddbe92e03021bff43ca
Nizomiddin <reactspringgood@gmail.com>
2024-12-22 09:00:32 +0500

    coupon admin add

--- apps/coupon/admin.py
+++ apps/coupon/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from coupon.models import Coupon
+
+
 # Register your models here.
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['id','created_by','code']
